973cea38c411f1bca28c4005fd4cb38d8a293f63
Franco Ortega <francopdx@gmail.com>
2020-12-15 17:20:17 -0800

    add POST test for Log model

---
+++ __tests__/logs.test.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+const Recipe = require('../lib/models/recipe');
+
+describe('recipe-lab routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end;
+  });
+
+  it('creates a log', async() => {
+    const recipe = await Recipe.insert(
+      {
+        name: 'cookies',
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ]
+      }
+    );
+    return await request(app)
+      .post('/api/v1/logs')
+      .send({
+        dateOfEvent: 'Sept 26, 2020',
+        notes: 'Sunny day.',
+        rating: 96,
+        recipeId: recipe.id
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          dateOfEvent: 'Sept 26, 2020',
+          notes: 'Sunny day.',
+          rating: 96,
+          recipeId: recipe.id
+        });
+      });
+  });
+
+});
