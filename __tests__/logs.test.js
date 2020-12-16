fae6a9b2434d73cbf7679d589c99a0fcd57ac96a
Franco Ortega <francopdx@gmail.com>
2020-12-15 17:57:38 -0800

    add GET all logs test for Log model

--- __tests__/logs.test.js
+++ __tests__/logs.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const Recipe = require('../lib/models/recipe');
+const Log = require('../lib/models/Log');
 
 describe('recipe-lab routes', () => {
   beforeEach(() => {
@@ -42,6 +43,38 @@ describe('recipe-lab routes', () => {
           recipeId: recipe.id
         });
       });
+  });
+
+
+  it('gets all logs', async() => {
+    const recipe = await Recipe.insert(
+      {
+        name: 'cookies',
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ]
+      }
+    );
+
+    const logs = await Promise.all([
+      { dateOfEvent: 'Sept 26, 2020', notes: 'Sunny day.', rating: 96, recipeId: recipe.id },
+      { dateOfEvent: 'March 9, 2020', notes: 'Wind and rain.', rating: 47, recipeId: recipe.id },
+      { dateOfEvent: 'Jan 2, 2020', notes: 'Lovely snow.', rating: 82, recipeId: recipe.id }
+    ].map(log => Log.insert(log)));
+
+    return await request(app)
+      .get('/api/v1/logs')
+      .then(res => {
+        logs.forEach(log => {
+          expect(res.body).toEqual(log);
+        });
+      });
+
+
+
   });
 
 });
