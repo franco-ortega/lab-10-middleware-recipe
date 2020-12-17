a5e306c2135b955686d8d8517e9a037808c2de3e
Franco Ortega <francopdx@gmail.com>
2020-12-17 13:52:26 -0800

    add uppercase to Recipe.js file name; rename recipes test file to be more accurate

--- __tests__/logs.test.js
+++ __tests__/logs.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
-const Recipe = require('../lib/models/recipe');
+const Recipe = require('../lib/models/Recipe');
 const Log = require('../lib/models/Log');
 
 describe('recipe-lab routes', () => {
